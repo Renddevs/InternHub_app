36861eca3afb47fc52d3f91bcdf28e9d0a13cf9f
Rendyscarlet123 <rendy9622@gmail.com>
2023-07-13 13:46:17 +0700

    add controller & model pembayaran kp

---
+++ app/Http/Controllers/PembayaranKpController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as BaseController;
+
+use Illuminate\Http\JsonResponse;
+
+class PembayaranKpController extends BaseController
+{
+    
+}
+
+?>
