137d3f4f157c9201c7ca7641c9972264dfbec9d6
Renddevs <rendy9622@gmail.com>
2023-09-27 10:40:19 +0700

    add migration & model trs_pengajuan_tkp

--- database/migrations/2023_09_27_033827_trs_pengajuan_tkp.php
+++ database/migrations/2023_09_27_033827_trs_pengajuan_tkp.php
@@ -11,23 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('trs_pengajuan_kp', function (Blueprint $table) {
+        Schema::create('trs_pengajuan_tkp', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('id_trs_pendaftaran_kp');
             $table->uuid('id_mst_mahasiswa');
-            $table->uuid('id_mst_media_sb');
             $table->string('nama_perusahaan');
             $table->string('nama_penanggung_jawab');
             $table->string('email_perusahaan');
             $table->string('noHp_penanggung_jawab');
-            $table->string('keterangan');
-            $table->string('create_by');
-            $table->datetime('created_date');
-            $table->string('update_by');
-            $table->datetime('updated_date');
+            $table->text('keterangan');
+            $table->datetime('tgl_pengajuan');
             $table->foreign('id_trs_pendaftaran_kp')->references('id')->on('trs_pendaftaran_kp');
             $table->foreign('id_mst_mahasiswa')->references('id')->on('mst_mahasiswa');
-            $table->foreign('id_mst_media_sb')->references('id')->on('mst_media');
         });
     }
 
@@ -36,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('trs_pengajuan_kp');
+        Schema::dropIfExists('trs_bimbingan_kp');
     }
 };
